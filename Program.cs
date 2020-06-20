61e935fbfad5a3febcd83d0d950a96a31aefbba8
mdtimshin <63156611+mdtimshin@users.noreply.github.com>
2020-06-20 22:18:41 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,76 +9,91 @@ namespace ex._2
     {
         static void Main(string[] args)
         {
-            string secondLine = File.ReadLines("input.txt").Skip(1).First();
-            string[] chisla = secondLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            int[] numbers = new int[chisla.Length];
-            for(int count=0; count<chisla.Length; count++)
+            try
             {
-                numbers[count] = int.Parse(chisla[count]);
-            }
-            List<int> cifri = new List<int>();
-            int[] sort = new int[numbers.Length];
-            numbers.CopyTo(sort, 0);
-            Array.Sort(sort);
-            int MAX = 0, CURRENT = 0, memory=numbers.Max();
-            for(int i=0; i<sort.Length-1; i++)
-            {
-                if (sort[i] == sort[i + 1])
+                string secondLine = File.ReadLines("input.txt").Skip(1).First();
+                string[] chisla = secondLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int[] numbers = new int[chisla.Length];
+                for (int count = 0; count < chisla.Length; count++)
                 {
-                    CURRENT++;
+                    numbers[count] = int.Parse(chisla[count]);
+                }
+                List<int> cifri = new List<int>();
+                int[] sort = new int[numbers.Length];
+                numbers.CopyTo(sort, 0);
+                Array.Sort(sort);
+                int MAX = 0, CURRENT = 0, memory = numbers.Max();
+                for (int i = 0; i < sort.Length - 1; i++)
+                {
+                    if (sort[i] == sort[i + 1])
+                    {
+                        CURRENT++;
+                    }
+                    else
+                    {
+                        if ((CURRENT > MAX) && (MAX != 0))
+                        {
+                            MAX = CURRENT;
+                            memory = sort[i];
+                        }
+                        if ((CURRENT > MAX) && (MAX == 0))
+                        {
+                            memory = sort[i];
+                            MAX = CURRENT;
+                        }
+                        if ((CURRENT == MAX) && (CURRENT != 0) && (MAX != 0))
+                        {
+                            if (memory > sort[i])
+                            {
+                                memory = sort[i];
+                            }
+                        }
+                        CURRENT = 0;
+                    }
+                }
+                if (MAX == 0)
+                {
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (numbers[i] != sort.Min())
+                        {
+                            cifri.Add(numbers[i]);
+                        }
+                    }
+                    cifri.Add(sort.Min());
                 }
                 else
                 {
-                    if ((CURRENT > MAX)&&(MAX!=0))
+                    for (int i = 0; i < numbers.Length; i++)
                     {
-                        MAX = CURRENT;
-                        memory = sort[i];
+                        if (numbers[i] != memory)
+                            cifri.Add(numbers[i]);
                     }
-                    if((CURRENT > MAX) && (MAX == 0))
+                    for (int a = 0; a <= MAX; a++)
                     {
-                        memory = sort[i];
-                        MAX = CURRENT;
+                        cifri.Add(memory);
                     }
-                    if ((CURRENT == MAX) && (CURRENT != 0) && (MAX != 0))
+                }
+                using (StreamWriter wr = new StreamWriter("output.txt"))
+                {
+                    foreach (int n in cifri)
                     {
-                        if (memory > sort[i])
-                        {
-                            memory = sort[i];
-                        }
+                        wr.Write(n + " ");
                     }
-                    CURRENT = 0;
                 }
             }
-            if (MAX == 0)
+            catch (System.FormatException)
             {
-                
-                for(int i = 0; i<numbers.Length; i++)
-                {
-                    if (numbers[i] != sort.Min())
-                    {
-                        cifri.Add(numbers[i]);
-                    }
-                }
-                cifri.Add(sort.Min());
+                Console.WriteLine("В последовательности содержатся литеры");
             }
-            else
+            catch (System.IO.FileNotFoundException)
             {
-                for(int i=0; i<numbers.Length; i++)
-                {
-                    if(numbers[i]!=memory)
-                    cifri.Add(numbers[i]);
-                }
-                for (int a = 0; a <= MAX; a++)
-                {
-                    cifri.Add(memory);
-                }
+                Console.WriteLine("Файл input.txt не найден");
             }
-            using(StreamWriter wr = new StreamWriter("output.txt"))
+            catch (System.InvalidOperationException)
             {
-                foreach(int n in cifri)
-                {
-                    wr.Write(n + " ");
-                }
+                Console.WriteLine("Файл input.txt пуст");
             }
         }
     }
